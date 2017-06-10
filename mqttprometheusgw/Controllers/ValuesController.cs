44198efe6909999c4107464ab730358c90d1cefa
cptnalf <captainalf@gmail.com>
2017-06-10 11:06:27 -0400

    add in mqtt gateway for prometheus
    * started, notes in valuescontroller.
      general plan outlined.
      will accept posted values for various channels

---
+++ mqttprometheusgw/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mqttprometheusgw.Controllers
+{
+  [Route("")]
+  public class ValuesController : Controller
+  {
+    // GET api/values
+    [HttpGet("metrics")]
+    public IActionResult Get()
+    {
+      string result = null;
+
+      /*
+       * expiring - values published to me need to expire at some point in time.
+       *     we want mutliple requests to give new results
+       *     , and we want values published to go away when the publishing sources go away
+       *     (maybe link to presence?)
+       * storage: need to link many channels (devices) to one metric with many instances.
+       *   mqtt channel => one device
+       *   eg:
+       *    house/serverroom/temp
+       *    house/garage/temp
+       *    
+       *    turned into:
+       *    esp8266_temp{serverroom}
+       *    esp8266_temp{garage}
+       * 
+       *   
+       * 
+       */
+
+      return Ok(result);
+    }
+  }
+}
